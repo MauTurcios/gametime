17326475f6ca7d37c71781d8c4728b1b413cdd4c
MauTurcios <51097339+MauTurcios@users.noreply.github.com>
2019-10-29 18:15:16 -0600

    Update agregar.php

--- agregar.php
+++ agregar.php
@@ -6,15 +6,15 @@ include_once 'db.php';
  * and open the template in the editor.
  */
 if(isset($_REQUEST["juego"])) {
-	$q="INSERT INTO `gametime` (`juego`, `plataforma`,`dia`,`inicio`,`fin`, `total_horas`)
-    VALUES (:p_juego, :p_plataforma, :p_dia, :p_inicio, :p_fin, :p_total_horas);";
+	$q="INSERT INTO `gametime` (`user`,`juego`, `plataforma`,`dia`,`inicio`,`fin`)
+    VALUES (:p_user,:p_juego, :p_plataforma, :p_dia, :p_inicio, :p_fin);";
     $pdo_statement=$pdo_conn->prepare($q);
-    $pdo_statement->bindParam(':p_juego', $_REQUEST['juego'], PDO::PARAM_STR);
-    $pdo_statement->bindParam(':p_plataforma', $_REQUEST['plataforma'], PDO::PARAM_STR);
+        $pdo_statement->bindParam(':p_user', $_REQUEST['user'], PDO::PARAM_STR);
+        $pdo_statement->bindParam(':p_juego', $_REQUEST['juego'], PDO::PARAM_STR);
+        $pdo_statement->bindParam(':p_plataforma', $_REQUEST['plataforma'], PDO::PARAM_STR);
 	$pdo_statement->bindParam(':p_dia', $_REQUEST['dia'], PDO::PARAM_STR);
 	$pdo_statement->bindParam(':p_inicio', $_REQUEST['inicio'], PDO::PARAM_STR);
 	$pdo_statement->bindParam(':p_fin', $_REQUEST['fin'], PDO::PARAM_STR);
-	$pdo_statement->bindParam(':p_total_horas', $_REQUEST['total_horas'], PDO::PARAM_STR);
     $result = $pdo_statement->execute();        
 	echo $result;
 }
